f608f65d560398019e57e4352d01a108bf31c5ba
yangxuan <realshineyang@gmail.com>
2016-12-14 12:47:48 +0800

    fix some bugs.

--- app/src/main/java/com/shineyang/scrapbook/activity/EditorActivity.java
+++ app/src/main/java/com/shineyang/scrapbook/activity/EditorActivity.java
@@ -13,7 +13,6 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
--- app/src/main/java/com/shineyang/scrapbook/utils/EditTextUtil.java
+++ app/src/main/java/com/shineyang/scrapbook/utils/EditTextUtil.java
@@ -2,15 +2,22 @@ package com.shineyang.scrapbook.utils;
 
 /**
  * Created by ShineYang on 2016/12/8
- * <p>
+ * BASEDD ON
  * Copyright 2016. SHENQINCI(沈钦赐)<946736079@qq.com>
- * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Copyright 2016. SHENQINCI(沈钦赐)<946736079@qq.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,21 +25,6 @@ package com.shineyang.scrapbook.utils;
  * limitations under the License.
  */
 
-/**
- * Copyright 2016. SHENQINCI(沈钦赐)<946736079@qq.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import android.support.annotation.NonNull;
 import android.text.Editable;
@@ -101,7 +93,11 @@ public class EditTextUtil {
         } else {
             //插销删除
             editable.insert(action.startCursor, action.actionTarget);
-            editText.setSelection(action.startCursor, action.endCursor);
+            if (action.endCursor == action.startCursor) {
+                editText.setSelection(action.startCursor + action.actionTarget.length());
+            } else {
+                editText.setSelection(action.startCursor, action.endCursor);
+            }
         }
         //释放操作
         flag = false;
